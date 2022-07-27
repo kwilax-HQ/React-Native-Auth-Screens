940d5023b2a57acbb2537eb5850e97219f4065c2
sh3riff <contactme@sh3riff.me>
2022-07-27 22:59:35 +0100

    fix build issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
 
 export default () => {
   return (
-    <View>
-        <Text>Auth Component</Text>
+    <View style={styles.body}>
+      <Text style={styles.text}>Auth Component</Text>
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: '700',
+    textAlign: 'center',
+    width: '80%',
+  },
+});
