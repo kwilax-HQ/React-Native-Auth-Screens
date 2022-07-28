0d2fc4ab115df107f6b80072b8c873f3313b780a
sh3riff <contactme@sh3riff.me>
2022-07-28 21:32:49 +0100

    Implementing Onboarding Screen

--- src/index.tsx
+++ src/index.tsx
@@ -6,15 +6,25 @@ import LoginScreen from './screens/Login';
 interface IAuthScreen {
   isAuthenticated?: boolean;
   children?: React.ReactNode;
+  config?: {
+    OnboardingScreen?: () => JSX.Element;
+  };
 }
 
 const Stack = createNativeStackNavigator();
 
-export default ({isAuthenticated, children}: IAuthScreen) => {
+export default ({isAuthenticated, children, config}: IAuthScreen) => {
+  const {OnboardingScreen} = config || {};
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{headerShown: false}}>
       {!isAuthenticated ? (
         <>
+          {OnboardingScreen ? (
+            <Stack.Screen
+              name="OnboardingScreen"
+              component={OnboardingScreen}
+            />
+          ) : null}
           <Stack.Screen name="Login" component={LoginScreen} />
         </>
       ) : (
